b801ec477c2ba4eabd924bbb5162573db93a720f
Jaime Kurzweg <jkurzweg10@gmail.com>
2023-02-25 16:59:26 -0700

    removed debug code

--- addons/EditorSwitcher/EditorSwitcher.gd
+++ addons/EditorSwitcher/EditorSwitcher.gd
@@ -8,7 +8,6 @@ var SwitchEditorButton = preload("EditorButton.gd")
 func _enter_tree():
 	var editor_node = get_tree().get_root().get_child(0)
 	var gui_base = editor_node.get_gui_base()
-	var icon = preload("icon.png")
 	var icon_activated = gui_base.get_icon("ScriptCreateDialog", "EditorIcons") #ToolConnect
 	var icon_unactivated = gui_base.get_icon("ScriptExtend", "EditorIcons")
 	
@@ -18,10 +17,8 @@ func _enter_tree():
 func _switch_pressed(is_activated:bool):
 	if is_activated:
 		get_editor_interface().get_editor_settings().set_setting("text_editor/external/use_external_editor", true)
-		print("hello")
 	else:
 		get_editor_interface().get_editor_settings().set_setting("text_editor/external/use_external_editor", false)
-		print("woah")
 	get_editor_interface().edit_script(
 		get_editor_interface().get_script_editor().get_current_script())
 	
